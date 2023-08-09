68508da9164cb5dc56fd34994510b5219818b330
Nilay Jain <nilayjain12@gmail.com>
2023-08-09 13:16:35 -0700

    Enabling microphone recording
    Enabling microphone recording to record the song.

--- abcd.py
+++ abcd.py
@@ -5,7 +5,7 @@ from shazamio import Shazam
 async def main():
   shazam = Shazam()
   out = await shazam.recognize_song(r'C:\Users\14089\Downloads\Aaj Jane Ki Zid Na Karo - (Raag.Fm).mp3')
-  print(out)
+  print(out['track']['title'])
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
